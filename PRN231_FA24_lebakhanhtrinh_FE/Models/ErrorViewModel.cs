a13eb7d6eff77ed6b3f8e5803cf927620e350aed
trinh <trinhbloc2003@gmail.com>
2024-11-03 15:06:26 +0700

    firet commit

---
+++ PRN231_FA24_lebakhanhtrinh_FE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRN231_FA24_lebakhanhtrinh_FE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
